0597791595efcaf2c300ec1fdb2e8afdbc9cf6e1
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-01 15:31:25 +0000

    file structure + python dependencies

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -9,6 +10,8 @@ CORS(app)
 def hello():
     return {'message': 'Hello from Flask!'}
 
+
+print(os.getenv("test"))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
